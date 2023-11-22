7547be0449878567b99e0b537c6013f9aa56a7c6
johnvmp275 <marquesjoaov1@gmail.com>
2023-11-22 16:57:01 -0300

    correção e adição do innerText

--- js/script.js
+++ js/script.js
@@ -1,12 +1,30 @@
-const sidebar = document.querySelector('.sidebar-nav')
-const switchToggle = document.querySelector('.toggle-switch')
-const body = document.querySelector('body')
-const buttonClose = document.querySelector('.sidebar-close')
+const sidebar = document.querySelector('.sidebar-nav');
+const switchToggle = document.querySelector('.toggle-switch');
+const body = document.querySelector('body');
+const buttonClose = document.querySelector('.sidebar-close');
+const modeSelect = document.querySelector('.sidebar-mode');
+
+//Add mode class to body
 
 switchToggle.addEventListener('click', () => {
-    body.classList.toggle('mode')
+  body.classList.toggle('mode');
+  modeContains();
 });
 
+//Add close class to the sidebar
+
 buttonClose.addEventListener('click', () => {
-    sidebar.classList.toggle('close')
+  sidebar.classList.toggle('close');
 });
+
+//Change span tag text
+
+function modeContains() {
+  if (body.classList.contains('mode')) {
+    modeSelect.innerText = 'Darkmode';
+  } else {
+    modeSelect.innerText = 'Lightmode';
+  }
+}
+
+modeContains();
